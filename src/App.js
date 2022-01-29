68c39b73bf91b5175fe98a79d98923272fe1bb5a
nverabgaryan <abgaryannwer@gmail.com>
2022-01-29 13:50:03 +0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import {
+    Switch,
+    Route,
+} from "react-router-dom";
 import {Home} from './pages/Home';
 import {Privacy} from './pages/Privacy';
 import {Login} from './pages/Login';
 
-function App() {
+export function App() {
   return (
     <div className="App">
-     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+        <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route path="/privacy">
+                <Privacy />
+            </Route>
+            <Route path="/login">
+                <Login />
+            </Route>
+        </Switch>
     </div>
   );
 }
-
-export default App;
