31f3e750ac982b7cac5d6fc9e5ee67b48686dc29
Ina <Ina_Grigorya@aybschool.am>
2022-05-21 14:57:03 +0400

    added user details

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,11 +8,8 @@ const BACKEND_URL = 'https://vms-ayb.herokuapp.com'
 const userLoginRequest = async (email, password) => {
     try {
         const response = await axios.post(`${BACKEND_URL}/auth/login`, {email, password})
-
         const access_token = response.data.access_token
-
         localStorage.setItem('token', access_token)
-
         return true
     } catch (e) {
         return false
@@ -24,32 +21,6 @@ export const Login = () => {
     const [login, setLogin] = useState("");
     const [passw, setPassw] = useState("");
     const [wrong, setWrong] = useState(false);
-    const users = [
-        {
-            username: "12345",
-            password: "qwerty",
-            id: 1,
-            name: "harut",
-            surname: "harutyunyan",
-            phone: "055555555"
-        },
-        {
-            username: "barev",
-            password: "dzez",
-            id: 2,
-            name: "ayo",
-            surname: "voch",
-            phone: "099999999"
-        },
-        {
-            username: "ok",
-            password: "ok",
-            id: 3,
-            name: "yes",
-            surname: "no",
-            phone: "077777777"
-        }
-    ];
 
     const LogIn = async (event) => {
         if (login !== "" && passw !== "") {
